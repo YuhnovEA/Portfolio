b9d261a5a033f818158a35e52d06dd6ea29d4fa0
@evgenii_iukhnov <evg1721@yandex.ru>
2023-06-04 23:36:39 +0800

    hero-section

---
+++ script/script.js
@@ -0,0 +1,4 @@
+const modalWindowBtn = document.querySelector('.hero__form__button')
+modalWindowBtn.addEventListener('click', () => {
+    document.querySelector('.modal-window').classList.toggle('none')
+})
