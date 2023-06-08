0f231f8078b349f6d6559c4cc16942366dcb1d4f
sergey <oborok05@bk.ru>
2023-06-08 07:53:49 +0300

    modal window fix

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 const modalWindowBtn = document.querySelector('.hero__form__button')
-modalWindowBtn.addEventListener('click', () => {
+modalWindowBtn.addEventListener('click', test)
+function test(event) {
+    event.preventDefault()
     document.querySelector('.modal-window').classList.toggle('none')
-})
+}
