d7ef5a1b5d1ee5296c98db1d90d9733eb032aa20
@evgenii_iukhnov <evg1721@yandex.ru>
2023-06-08 21:12:23 +0800

    work two

--- script/script.js
+++ script/script.js
@@ -2,3 +2,16 @@ const modalWindowBtn = document.querySelector('.hero__form__button')
 modalWindowBtn.addEventListener('click', () => {
     document.querySelector('.modal-window').classList.toggle('none')
 })
+// -------swiper------
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
+  
