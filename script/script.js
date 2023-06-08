00256b4f9f4583ba44001f0b343975058914fc07
@evgenii_iukhnov <evg1721@yandex.ru>
2023-06-08 22:27:46 +0800

    work two

--- script/script.js
+++ script/script.js
@@ -1,17 +1,19 @@
 const modalWindowBtn = document.querySelector('.hero__form__button')
-modalWindowBtn.addEventListener('click', () => {
+modalWindowBtn.addEventListener('click', test)
+function test(event) {
+    event.preventDefault()
     document.querySelector('.modal-window').classList.toggle('none')
-})
+}
 // -------swiper------
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    //direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    }
-  });
-  
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   //direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true
+//   }
+// });
+  new Accordion('.accordion-container')
