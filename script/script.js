22d8d96b99bc2e3ef0039ac4e6674a360864141c
@evgenii_iukhnov <evg1721@yandex.ru>
2023-06-09 18:05:58 +0800

    work 4

--- script/script.js
+++ script/script.js
@@ -4,16 +4,15 @@ function test(event) {
     event.preventDefault()
     document.querySelector('.modal-window').classList.toggle('none')
 }
-// -------swiper------
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   //direction: 'vertical',
-//   loop: true,
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true
-//   }
-// });
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
   new Accordion('.accordion-container')
